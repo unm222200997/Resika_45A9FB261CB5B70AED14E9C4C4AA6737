1cb22d71099945675efa162c2e7b47c1cc01e803
unm222200997 <144539182+unm222200997@users.noreply.github.com>
2023-09-09 06:17:59 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+def fact_rec(n):
+  if n==0 or n==1:
+    return True
+  else:
+    return n* fact_rec(n-1)
+number=int(input("Enter a Value:"))
+res=fact_rec(number)
+print ("The factorial of {}is {}."
+       .format (number,res))
